1f929bc561ce60aed7dc7582b719559176b1713e
VedBisne10 <roronoazoro73511@gmail.com>
2026-08-31 09:35:05 +0530

    Update server log message with URL

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.delete("/api/companies/:id", (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000: http://localhost:3000");
 });
 
 
